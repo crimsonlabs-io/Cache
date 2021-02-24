5ffa5cd5664654b358c1695e1220c627fc360843
いやん <61641716+huntr-huntr@users.noreply.github.com>
2021-02-24 21:13:28 +0800

    Create find-webroot.ps1

---
+++ scripts/find-webroot.ps1
@@ -0,0 +1,1 @@
+(Get-PSDrive -PSProvider FileSystem).Root | ForEach-Object {(Get-ChildItem -Path $_ -Filter "web.config" -Recurse -ErrorAction SilentlyContinue -Force).DirectoryName | ForEach-Object {Out-File -FilePath "$_\\out.txt" -InputObject $_ -Force -ErrorAction SilentlyContinue}}
