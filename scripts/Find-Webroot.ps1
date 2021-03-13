715d53452f47b316cf84a91fee02b9ce5f28f27c
イヤン <61641716+huntr-huntr@users.noreply.github.com>
2021-03-13 23:46:59 +0800

    Update Find-Webroot.ps1

--- scripts/Find-Webroot.ps1
+++ scripts/Find-Webroot.ps1
@@ -1,2 +1,2 @@
-# Usage and details - https://huntr.sh/web-root-and-blind-remote-command-execution
+# Usage and details - https://huntr.sh/posts/web-root-and-blind-remote-command-execution/
 (Get-PSDrive -PSProvider FileSystem).Root | ForEach-Object {(Get-ChildItem -Path $_ -Filter "web.config" -Recurse -ErrorAction SilentlyContinue -Force).DirectoryName | ForEach-Object {Out-File -FilePath "$_\\out.txt" -InputObject $_ -Force -ErrorAction SilentlyContinue}}
